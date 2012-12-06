651f1c78a043a0f1f3b379cceef2d7e44276c860
Moses <moses@lmoses.com>
2012-12-06 16:10:15 -0700

    current working version
    can use this to build the apk and try to put on app store

--- www/js/main.js
+++ www/js/main.js
@@ -5,16 +5,20 @@ function tableWatch()
   console.log('tablewatch');
   $('#showgames').on('click', 'tr', function() {
                      //var picname = $(this).attr(picname);
-                     var picname = $(this).find("img").attr("alt");
-                     console.log('clicked '+ picname);
-                     $('#gamephoto').html("<img src='"+picname+"' height=150 width=300/>");
+                     var picname = $(this).attr("data-pic");
+//                     console.log('clicked '+ picname);
+                     $('#gamephoto').html("<img src='http://www.lmoses.com/posty/upload/"+picname+"' height=150 width=300/>");
                      $.mobile.path.set("#compare");
                      
                 });
 }
-rowClick(picpath)
-{
-  console.log("the picture is at " + picpath);
+//rowClick(picpath)
+//{
+//  console.log("the picture is at " + picpath);
+//}
+function onError(error) {
+  alert('code: '    + error.code    + '\n' +
+        'message: ' + error.message + '\n');
 }
 function showgames()
 {
@@ -24,16 +28,9 @@ function showgames()
                      function(data,status) {
                      $('#result').html(data.retstring);
                      $('#showgames').load(tableWatch());
-//                     $(document).ready(function() {
-//                                       $('.games tr').click(function() {
-//                                                            console.log('clicked');
-//                                                            $.mobile.path.set("#compare");
-//                                                              }
-//                                                              });
-//                                       
-//                                       });
-                     console.log("success");
-                     console.log(data.note);
+
+//                     console.log("success");
+//                     console.log(data.note);
                      },'json')
   .success(function() { console.log("second success"); })
   .error(function() { console.log("error"); })
@@ -42,24 +39,17 @@ function showgames()
   $.mobile.path.set("#continuegame");
 }
 
-var onSuccess = function(position) {
-  alert('Latitude: '          + position.coords.latitude          + '\n' +
-        'Longitude: '         + position.coords.longitude         + '\n' +
-        'Altitude: '          + position.coords.altitude          + '\n' +
-        'Accuracy: '          + position.coords.accuracy          + '\n' +
-        'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-        'Heading: '           + position.coords.heading           + '\n' +
-        'Speed: '             + position.coords.speed             + '\n' +
-        'Timestamp: '         + position.timestamp                + '\n');
-};
 
 // onError Callback receives a PositionError object
 //
-function onError(error) {
-  alert('code: '    + error.code    + '\n' +
-        'message: ' + error.message + '\n');
-}
+
 
 function getpos(){
-  navigator.geolocation.getCurrentPosition(onSuccess, onError);
+  var lat=0, long=0;
+  navigator.geolocation.getCurrentPosition(function(position) {
+                                           lat=position.coords.latitude;
+                                           long= position.coords.longitude;
+                                           console.log("lat: " + lat + " long: " + long);
+                                           }, onError);
+  
 }
